69284530f296a73a77e0113279722bbc2c047f18
Mahan <khalili1388mahan@gmail.com>
2025-02-09 12:48:08 +0330

    Add GlobalPosition.gd

---
+++ GlobalPosition.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@export var target: Node
+@export var output: Label
+@export var use_custom_output: bool = false
+
+var _label: Label
+
+func _ready() -> void:
+    _label = Label.new()
+    add_child(_label)
+    if use_custom_output and output:
+            _label = output
+    if !target:
+        target = self
+
+func _process(_delta: float) -> void:
+    output.text = "%d, %d" % [target.position.x, target.position.y]
